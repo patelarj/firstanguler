60640a27868024b564747329bb47d08d75990146
Arjun <65750208+patelarj@users.noreply.github.com>
2020-07-31 18:54:50 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,10 @@
+import { Books} from '../../app/bookInfo';
+
+ export const MYCLASSES: Books[] = 
+ [
+    {bname: "Intro to Algo", bauthor: "Thomas h", bgenra: "Algorithms", byearpublished: "1989", bpicture: "assets/image/image1.jpg"},
+    {bname: "The Algorithm Design Manual", bauthor: "Steven Skiena", bgenra: "Algorithms", byearpublished: "1997", bpicture: "assets/image/image2.jpeg"},
+    {bname: "The C Programming", bauthor: "Brian Kernighan and Dennis Ritchie", bgenra: "C Programming", byearpublished: "1978", bpicture: "assets/image/image3.jpeg"},
+    {bname: "Head First Java", bauthor: "Bert Bates and Kathy Sierra", bgenra: "java", byearpublished: "2003", bpicture: "assets/image/image4.jpeg"},
+
+ ]
